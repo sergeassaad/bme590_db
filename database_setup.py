e122d977438ce83a423f3698395eb96f23133f96
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-11-09 16:37:13 -0500

    wrote Patient class, setup webservices script

---
+++ database_setup.py
@@ -0,0 +1,9 @@
+from pymodm import connect
+from pymodm import MongoModel, fields
+
+connect("mongodb://vcm-1834.vm.duke.edu:27017/bme590_db")
+
+class Patient(MongoModel):
+    name = fields.CharField(primary_key = True)
+    bmi = fields.FloatField()
+    age = fields.IntField()
---
+++ webservices_db.py
@@ -0,0 +1,21 @@
+from database_setup.py import Patient
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route("/average_bmi/<age>")
+def average_bmi(age):
+    sum = 0
+    count = 0
+    for pat in Patient.objects.raw({"age": age}):
+        count+=1
+        sum += pat.bmi
+    dict_out ={"average_bmi": float(sum/count)}
+    return jsonify(dict_out)
+
+
+@app.route("/new_patient", methods = ['POST'])
+def new_patient():
+    if type(request.json['name']) is not str or type(request.json['age']) is not int or type(request.json['bmi']) is not float:
+        request.status_code = 406  #validation error status code
+    u = Patient(request.json['name'], bmi=request.json['bmi'], age=request.json['age'])
+    u.save()
