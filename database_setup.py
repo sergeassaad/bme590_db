6900fa75227248fe111a013ec994a63d5e4c1e1b
Serge Assaad <sergeassaad@Serges-MacBook-Pro-2.local>
2017-11-09 16:39:46 -0500

    changed IntField to FloatField

--- database_setup.py
+++ database_setup.py
@@ -6,4 +6,4 @@ connect("mongodb://vcm-1834.vm.duke.edu:27017/bme590_db")
 class Patient(MongoModel):
     name = fields.CharField(primary_key = True)
     bmi = fields.FloatField()
-    age = fields.IntField()
+    age = fields.FloatField()
